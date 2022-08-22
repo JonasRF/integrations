f55b5e2081d54164d0a0a730911cd431ba1113e7
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-08-22 20:10:20 -0300

    Send Email test EmmailDTO

---
+++ src/main/java/com/devsuperior/integrations/dto/EmailDTO.java
@@ -0,0 +1,82 @@
+package com.devsuperior.integrations.dto;
+
+import java.io.Serializable;
+
+public class EmailDTO implements Serializable {
+    private String fromEmail;
+    private String fromName;
+    private String replayTo;
+    private String to;
+    private String subject;
+    private String body;
+    private String contentType;
+
+    public EmailDTO (){
+    }
+
+    public EmailDTO(String fromEmail, String fromName, String replayTo, String to, String subject, String body, String contentType) {
+        this.fromEmail = fromEmail;
+        this.fromName = fromName;
+        this.replayTo = replayTo;
+        this.to = to;
+        this.subject = subject;
+        this.body = body;
+        this.contentType = contentType;
+    }
+
+    public String getFromEmail() {
+        return fromEmail;
+    }
+
+    public void setFromEmail(String fromEmail) {
+        this.fromEmail = fromEmail;
+    }
+
+    public String getFromName() {
+        return fromName;
+    }
+
+    public void setFromName(String fromName) {
+        this.fromName = fromName;
+    }
+
+    public String getReplayTo() {
+        return replayTo;
+    }
+
+    public void setReplayTo(String replayTo) {
+        this.replayTo = replayTo;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
